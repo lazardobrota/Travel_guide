05b797c34c9f681faae413ebc2a1d31b6053952e
Lazar Dobrota <lazardobrota1323@gmail.com>
2024-05-29 22:48:19 +0200

    frontend: add new user form

---
+++ frontend/travel-guide/src/app/users/new/page.jsx
@@ -0,0 +1,91 @@
+"use client"
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+
+function User() {
+  this.email = ""
+  this.userTypeId = 2
+  this.active = true
+  this.password = ""
+  this.firstname = ""
+  this.lastname = ""
+}
+
+export default function NewUser() {
+  const [user, setUser] = useState(new User())
+  const [userTypes, setUserTypes] = useState(undefined)
+  const router = useRouter()
+
+  const jwtCookie = 'TODO'
+
+  useEffect(() => {
+    fetch('http://localhost:8081/api/user/type', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + jwtCookie
+      }
+    })
+      .then(res => res.json())
+      .then(data => setUserTypes(data))
+  }, [])
+
+  function handleSubmit(e, user) {
+    e.preventDefault()
+
+    //admin must be active
+    if (user.userTypeId === 1)
+      user.active = true
+
+    console.log(user)
+    fetch('http://localhost:8081/api/user', {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Bearer ' + jwtCookie,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: user.email,
+        userTypeId: user.userTypeId,
+        active: user.active,
+        password: user.password,
+        name: user.firstname,
+        lastname: user.lastname
+      })
+    })
+    .then(res => router.push('/users'))
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+  }
+
+  if (userTypes === undefined)
+    return
+
+  return (
+    <form onSubmit={e => handleSubmit(e, user)}>
+      <div>
+        <label>Name: </label>
+        <input required name="name" value={user.firstname} onChange={e => setUser({ ...user, firstname: e.target.value })} />
+
+        <label>Lastname: </label>
+        <input required name="lastname" value={user.lastname} onChange={e => setUser({ ...user, lastname: e.target.value })} />
+      </div>
+      <div>
+        <label>Email: </label>
+        <input required name="email" value={user.email} onChange={e => setUser({ ...user, email: e.target.value })} />
+
+        <label>Password: </label>
+        <input required name="password" value={user.password} onChange={e => setUser({ ...user, password: e.target.value })} />
+      </div>
+      <div>
+        <select defaultValue={2} onChange={(e) => setUser({ ...user, userTypeId: parseInt(e.target.value) })}>
+          {userTypes.map((elem) => (
+            <option key={elem.id} value={elem.id}>{elem.role}</option>
+          ))}
+        </select>
+        <label hidden={user.userTypeId === 1 ? true : false}>Active: </label>
+        <input hidden={user.userTypeId === 1 ? true : false} name="type" type="checkbox" defaultChecked={user.active} onChange={e => setUser({ ...user, active: !user.active })} />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
